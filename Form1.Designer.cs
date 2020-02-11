ecacf2c4dd3c67429857eff372108e91854fbd3d
jemmyPC <54220935+jemmyPC@users.noreply.github.com>
2020-02-11 00:53:51 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DPRN1_U1_EA_JEPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSalida = new System.Windows.Forms.ComboBox();
+            this.comboBoxEntrada = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.salida = new System.Windows.Forms.TextBox();
+            this.ingreso = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Conversor de Temperatura";
+            // 
+            // comboBoxSalida
+            // 
+            this.comboBoxSalida.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxSalida.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBoxSalida.DropDownHeight = 120;
+            this.comboBoxSalida.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSalida.DropDownWidth = 180;
+            this.comboBoxSalida.FormattingEnabled = true;
+            this.comboBoxSalida.IntegralHeight = false;
+            this.comboBoxSalida.ItemHeight = 13;
+            this.comboBoxSalida.Items.AddRange(new object[] {
+            "Celsius",
+            " farenhait",
+            "kelvin"});
+            this.comboBoxSalida.Location = new System.Drawing.Point(315, 103);
+            this.comboBoxSalida.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBoxSalida.Name = "comboBoxSalida";
+            this.comboBoxSalida.Size = new System.Drawing.Size(202, 21);
+            this.comboBoxSalida.TabIndex = 5;
+            this.comboBoxSalida.SelectedIndexChanged += new System.EventHandler(this.comboBoxSalida_SelectedIndexChanged);
+            // 
+            // comboBoxEntrada
+            // 
+            this.comboBoxEntrada.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxEntrada.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBoxEntrada.DropDownHeight = 120;
+            this.comboBoxEntrada.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxEntrada.DropDownWidth = 180;
+            this.comboBoxEntrada.FormattingEnabled = true;
+            this.comboBoxEntrada.IntegralHeight = false;
+            this.comboBoxEntrada.ItemHeight = 13;
+            this.comboBoxEntrada.Items.AddRange(new object[] {
+            "Celsius",
+            "farenhait",
+            "kelvin"});
+            this.comboBoxEntrada.Location = new System.Drawing.Point(15, 100);
+            this.comboBoxEntrada.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBoxEntrada.Name = "comboBoxEntrada";
+            this.comboBoxEntrada.Size = new System.Drawing.Size(194, 21);
+            this.comboBoxEntrada.TabIndex = 6;
+            this.comboBoxEntrada.SelectedIndexChanged += new System.EventHandler(this.comboBoxEntrada_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(236, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 67);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "=";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // salida
+            // 
+            this.salida.Location = new System.Drawing.Point(316, 65);
+            this.salida.Name = "salida";
+            this.salida.Size = new System.Drawing.Size(201, 20);
+            this.salida.TabIndex = 8;
+            // 
+            // ingreso
+            // 
+            this.ingreso.Location = new System.Drawing.Point(15, 66);
+            this.ingreso.Name = "ingreso";
+            this.ingreso.Size = new System.Drawing.Size(194, 20);
+            this.ingreso.TabIndex = 9;
+            this.ingreso.TextChanged += new System.EventHandler(this.ingreso_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(555, 148);
+            this.Controls.Add(this.ingreso);
+            this.Controls.Add(this.salida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxEntrada);
+            this.Controls.Add(this.comboBoxSalida);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular Grados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSalida;
+        private System.Windows.Forms.ComboBox comboBoxEntrada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox salida;
+        private System.Windows.Forms.TextBox ingreso;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DPRN1_U1_EA_JEPC
+{
+    public partial class Form1 : Form
+    {
+
+        public double farenhait = 32;
+        public double celsius = 0;
+        public double kelvin = 273.15;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        private void Resolver()
+        {
+            if (ingreso.Text != "" && ingreso.Text != "-")
+            {
+                double dato = Convert.ToDouble(ingreso.Text);
+                int valIngreso = comboBoxEntrada.SelectedIndex;
+                int valSalida = comboBoxSalida.SelectedIndex;
+
+                // 0 = celcius  
+                // 1 = Fare
+                // 2 = Kelvin
+                switch (valIngreso)
+                {
+                    case 0:
+
+                        if (valSalida == 0)
+                            comboBoxEntrada.SelectedIndex = 1;
+
+                        if (valSalida == 1)
+                            salida.Text = (dato * 1.8 + 32).ToString();
+
+                        if (valSalida == 2)
+                            salida.Text = (dato * 273.15 + 32).ToString();
+                        break;
+
+                    case 1:
+
+                        if (valSalida == 0)
+                            salida.Text = ((dato - 32) / 1.8).ToString();
+
+                        if (valSalida == 1)
+                            comboBoxEntrada.SelectedIndex = 2;
+
+                        if (valSalida == 2)
+                            salida.Text = (((dato - 32) / 1.8) + 273.15).ToString();
+                        break;
+                    case 2:
+
+                        if (valSalida == 0)
+                            salida.Text = (dato - 273.15).ToString();
+
+                        if (valSalida == 1)
+                            salida.Text = ((dato - 273.15) * 1.8 + 32).ToString();
+
+                        if (valSalida == 2)
+                            comboBoxEntrada.SelectedIndex = 0;
+                        break;
+                }
+            }
+            else
+            {
+                salida.Clear();
+            }
+        }
+
+        private void ingreso_TextChanged(object sender, EventArgs e)
+        {
+            Resolver();
+        }
+
+        private void comboBoxEntrada_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desea hacer otra converion?");
+            Resolver();
+        }
+
+        private void comboBoxSalida_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desea hacer otra converion?");
+            Resolver();
+        }
+
+      
+    }
+}
+
